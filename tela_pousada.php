2726339a72d547b6f8909400d445f875d4e70a78
Alexandre <94768326+alexandredev45@users.noreply.github.com>
2023-09-06 16:37:31 -0300

    .

--- tela_pousada.php
+++ tela_pousada.php
@@ -9,11 +9,35 @@
     <title>Pousada</title>
 </head>
 <body>
-    <header>
-        <?php
-            require_once 'menu.php';
-        ?>
-    </header>
+<header>
+  <nav class="navbar navbar-expand-lg bg-body-tertiary">
+      <div class="container-fluid">
+        <a class="navbar-brand" href="#">Bem Vindo!!!</a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+          <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+            <li class="nav-item">
+              <a class="nav-link" href="#">Pedidos</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">Pousada</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">Produto</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">Passeio</a>
+            </li>
+          </ul>
+          <form class="d-flex" role="search">
+            <input class="form-control me-2" type="search" placeholder="Pesquisar" aria-label="Search">
+            <button class="btn btn-outline-success" type="submit">Pesquisar</button>
+          </form>
+        </div>
+      </div>
+    </nav>
 
     <main>
     </main>

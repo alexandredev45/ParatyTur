bf3612b0223170e292137e6d94f536b6f74a79e4
Sabrinaallgalvao <101464580+Sabrinaallgalvao@users.noreply.github.com>
2023-09-06 16:56:27 -0300

    teste em movimento/login
    criei o básico, falta integrar

--- movimento/login.php
+++ movimento/login.php
@@ -1,1 +1,18 @@
-<?php ?>
+<?php
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    $usuario = $_POST['usuario'];
+    $email = $_POST['email'];
+    $senha = $_POST['senha'];
+
+    // Verificar credenciais
+    $usuarioValido === true;
+
+    if ($usuarioValido) {
+        // Redirecionar para form.php com os valores
+        header("Location: form.php?usuario=$usuario&email=$email&senha=$senha");
+        exit();
+    } else {
+        echo "Credenciais inválidas. Por favor, tente novamente.";
+    }
+}
+?>

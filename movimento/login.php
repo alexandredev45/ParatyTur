4e75113baa07e55064fdf3950a3ec1cec30bdabd
Sabrinaallgalvao <101464580+Sabrinaallgalvao@users.noreply.github.com>
2023-09-08 17:33:50 -0300

    .

--- movimento/login.php
+++ movimento/login.php
@@ -1,17 +1,18 @@
 <?php
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-    $usuario = $_POST['usuario'];
-    $email = $_POST['email'];
-    $senha = $_POST['senha'];
+    $username = $_POST['username'];
+    $password = $_POST['password'];
 
-    // Verificar credenciais
-    $usuarioValido === true;
+    // Adicionar a lógica de verificação de credenciais.
+    //Verifica se o nome de usuário e a senha estão corretos.
+    // Substituir a lógica abaixo pela implementação real de autenticação.
 
-    if ($usuarioValido) {
-        // Redirecionar para form.php com os valores
-        header("Location: form.php?usuario=$usuario&email=$email&senha=$senha");
+    if ($username === 'usuario' && $password === 'senha') {
+        // Credenciais corretas, redirecionando para outra página.
+        header("Location: index.html");
         exit();
     } else {
+        // Caso credenciais inválidas, exiba uma mensagem de erro.
         echo "Credenciais inválidas. Por favor, tente novamente.";
     }
 }

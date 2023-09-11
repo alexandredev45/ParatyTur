4a7434f0f0951dfa84d424352c43471968057687
Sabrinaallgalvao <101464580+Sabrinaallgalvao@users.noreply.github.com>
2023-09-11 16:02:41 -0300

    login
    conexao com o bd

--- movimento/login.php
+++ movimento/login.php
@@ -3,12 +3,12 @@ include_once("../classes/conexao.php");
 
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 
-    $host = "localhost";
-    $user = "root";
-    $senha = "";
-    $dbname = "ParatyTur";
+    $dbhost = "localhost";
+    $dbUsername = "esocialu_AlexandreDev45";
+    $dbPassword = "8416RWo{bXM-";
+    $dbName     = "esocial";
 
-    $conexao = new mysqli($host, $user, $senha, $dbname);
+    $conexao = new mysqli($dbHost, $dbUsername, $dbPassword, $dbName);
 
     // Verificar conexão
     if ($conexao->connect_error) {
@@ -21,9 +21,9 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     // Lógica de verificação de credenciais.
     // Consulta SQL para inserir as credenciais no banco de dados.
 
-    $mysql = "INSERT INTO 'login' ('id_login', 'usuario', 'senha') VALUES (NULL, 'adm', '1234')";
+    $mysql = "INSERT INTO `login` (`id_login`, `usuario`, `senha`) VALUES (NULL, 'adm', '1234')";
 
-    if ($conexao->query($mysql) === TRUE) {
+    if (mysqli_query($conexao, $mysql)) {
         // Credenciais inseridas com sucesso.
         echo "Registro inserido com sucesso.";
         //header("Location: tela_adm.html"); // Caso esteja tudo certo, redireciona para tela_adm.html
@@ -32,9 +32,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
         // Caso ocorra algum erro ao inserir as credenciais, exiba uma mensagem de erro.
         echo "Erro ao inserir registro: " . $conexao->error;
     }
-
     // Fechar a conexão com o banco de dados
     $conexao->close();
 }
-
 ?>

a775814185ac1514bf71d767588b42b01e6aec95
Alexandre <94768326+alexandredev45@users.noreply.github.com>
2023-09-11 15:53:01 -0300

    .

--- classes/conexao.php
+++ classes/conexao.php
@@ -1,4 +1,49 @@
 <?php
+
+
+include_once("../classes/conexao.php");
+
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+
+    $dbHost     = "localhost";
+    $dbUsername = "esocialu_AlexandreDev45";
+    $dbPassword = "8416RWo{bXM-";
+    $dbName     = "esocialu_ParatyTur";
+
+    
+    $conexao = new mysqli($dbHost, $dbUsername, $dbPassword, $dbName);
+
+    // Verificar conexão
+    if ($conexao->connect_error) {
+        die("Erro de conexão com o banco de dados: " . $conexao->connect_error);
+    }
+
+    // Lógica de verificação de credenciais.
+    // Consulta SQL para inserir as credenciais no banco de dados.
+
+    $mysql = "INSERT INTO login (id_login, usuario, senha) VALUES (1, 'adm', '1234')";
+
+    if (mysqli_query($conexao, $mysql)) {
+        // Credenciais inseridas com sucesso.
+        echo "Registro inserido com sucesso.";
+        //header("Location: tela_adm.html"); // Caso esteja tudo certo, redireciona para tela_adm.html
+        exit();
+    } else {
+        // Caso ocorra algum erro ao inserir as credenciais, exiba uma mensagem de erro.
+        echo "Erro ao inserir registro: " . $conexao->error;
+    }
+
+    // Fechar a conexão com o banco de dados
+    $conexao->close();
+}
+
+
+
+
+
+
+
+/*
     define('HOST', 'localhost');
     define('DBNAME', 'esocialu_ParatyTur');
     define('USER', 'esocialu_AlexandreDev45');
@@ -24,5 +69,5 @@
         } 
 
     }
-    
+    */
 ?>

46e593d83b89310114a698233a716e7bc603f6f3
Sabrinaallgalvao <101464580+Sabrinaallgalvao@users.noreply.github.com>
2023-09-11 14:00:03 -0300

    estrutura
    estrutura de mov barco

--- movimento/movimento_barco.php
+++ movimento/movimento_barco.php
@@ -13,14 +13,14 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     if ($conexao->connect_error) {
         die("Erro de conexão com o banco de dados: " . $conexao->connect_error);
     }
-    //
+    
     $nome = $_POST['nome'];
     $proprietario = $_POST['proprietario'];
     $comprimento = $_POST['comprimento'];
     $capacidade = $_POST['capacidade'];
     $ano = $_POST['ano'];
-
-    $sql = "INSERT INTO 'barco' ('nome', 'proprietario', 'comprimento', 'capacidade', 'ano') VALUES ('$nome', '$proprietario', '$comprimento', '$capacidade', '$ano')";
+    //CADASTRA novo barco
+    $sql = "INSERT INTO `barco` (`nome`, `proprietario`, `comprimento`, `capacidade`, `ano`) VALUES ('$nome', '$proprietario', '$comprimento', '$capacidade', '$ano')";
 
     if ($conexao->query($mysql) === TRUE){
         echo "Seu barco foi registrado com sucesso."; // sucesso ao inserir o novo barco no banco de dados
@@ -30,35 +30,43 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     }
 }
 
-    $sql = "SELECT * FROM 'barco'";
+    //EXIBE listas de barco para EDIÇÃO e EXCLUSÃO
+if (isset($_GET['editar'])) {
+    $barco_id = $_GET['editar'];
 
+    $sql = "SELECT * FROM `barco` WHERE `id_barco` = $barco_id";
+    
     $resultado = $conexao->query($mysql);
-    
-    if ($resultado->num_rows > 0) {
-        // Exibir os barcos registrados em uma tabela HTML (mesma lógica de exibição que você já tem)
-        echo "<table>";
-        echo "<tr><th>ID</th><th>Nome</th><th>Proprietário</th><th>Comprimento</th><th>Capacidade</th><th>Ano de </th></tr>";
 
-    // Loop para exibir os registros
-    while ($linha = $resultado->fetch_assoc()) {
-        echo "<tr>";
-        echo "<td>" . $linha['id_barco'] . "</td>";
-        echo "<td>" . $linha['nome'] . "</td>";
-        echo "<td>" . $linha['proprietario'] . "</td>";
-        echo "<td>" . $linha['comprimento'] . "</td>";
-        echo "<td>" . $linha['capacidade'] . "</td>";
-        echo "<td>" . $linha['ano'] . "</td>";
-        echo "</tr>";
-    }
-        echo "</table>";
+    echo "<table>";
+    echo "<tr><th>ID</th><th>Nome</th><th>Proprietário</th><th>Comprimento</th><th>Capacidade</th><th>Ano</th><th>Ações</th></tr>";
+
+while ($linha = $resultado->fetch_assoc()) {
+    echo "<tr>";
+    echo "<td>" . $linha['id_barco'] . "</td>";
+    echo "<td>" . $linha['nome'] . "</td>";
+    echo "<td>" . $linha['proprietario'] . "</td>";
+    echo "<td>" . $linha['comprimento'] . "</td>";
+    echo "<td>" . $linha['capacidade'] . "</td>";
+    echo "<td>" . $linha['ano'] . "</td>";
+    echo "<td><a href='?editar=" . $linha['id_barco'] . "'>Editar</a> | <a href='?excluir=" . $linha['id_barco'] . "'>Excluir</a></td>";
+    echo "</tr>";
+}
+    echo "</table>";
+
+} elseif (isset($_GET['excluir'])) {
+    $barco_id = $_GET['excluir'];
+
+    $sql = "DELETE FROM `barco` WHERE `id_barco` = $barco_id";
+
+    if ($conexao->query($mysql) === TRUE) {
+        echo "Barco excluído com sucesso.";
     } else {
-        echo "Nenhum barco registrado.";
+        echo "Erro ao excluir o barco: " . $conexao->error;
     }
-    
-
-
-
-
+} else {
+    echo "Nenhum barco registrado.";
+}
     $conexao->close();// Fechar a conexão com o banco de dados
 
 ?>

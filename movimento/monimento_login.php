fe7737cdeaef32ec02991ac0ff564cfe41375e8e
Alexandre <94768326+alexandredev45@users.noreply.github.com>
2023-09-11 11:39:35 -0300

    .

---
+++ movimento/monimento_login.php
@@ -0,0 +1,40 @@
+<?php
+include_once("index.php");
+
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+
+    $host = "localhost";
+    $user = "root";
+    $senha = "";
+    $dbname = "ParatyTur";
+
+    $conexao = new mysqli($host, $user, $senha, $dbname);
+
+    // Verificar conexão
+    if ($conexao->connect_error) {
+        die("Erro de conexão com o banco de dados: " . $conexao->connect_error);
+    }
+
+    $usuario = 'adm';
+    $senha = '1234';
+
+    // Lógica de verificação de credenciais.
+    // Consulta SQL para inserir as credenciais no banco de dados.
+
+    $mysql = "INSERT INTO 'login' ('id_login', 'usuario', 'senha') VALUES (NULL, 'adm', '1234')";
+
+    if ($conexao->query($mysql) === TRUE) {
+        // Credenciais inseridas com sucesso.
+        echo "Registro inserido com sucesso.";
+        //header("Location: index.html"); // Caso esteja tudo certo, redireciona para index.html
+        exit();
+    } else {
+        // Caso ocorra algum erro ao inserir as credenciais, exiba uma mensagem de erro.
+        echo "Erro ao inserir registro: " . $conexao->error;
+    }
+
+    // Fechar a conexão com o banco de dados
+    $conexao->close();
+}
+
+?>
--- movimento/movimento_barco.php
+++ movimento/movimento_barco.php
@@ -1,1 +1,31 @@
-<?php ?>
+<?php
+
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    //conexao com o banco
+    $host = "localhost";
+    $user = "root";
+    $senha = "";
+    $dbname = "ParatyTur";
+
+    $conexao = new mysqli($host, $user, $senha, $dbname);
+    
+    if ($conexao->connect_error) {
+        die("Erro de conexão com o banco de dados: " . $conexao->connect_error);
+    }
+    //
+    $nome = $_POST['nome'];
+    $proprietario = $_POST['proprietario'];
+    $comprimento = $_POST['comprimento'];
+    $capacidade = $_POST['capacidade'];
+    $ano = $_POST['ano'];
+
+    $sql = "INSERT INTO 'barcos' ('nome', 'proprietario', 'comprimento', 'capacidade', 'ano') VALUES ('$nome', '$proprietario', '$comprimento', '$capacidade', '$ano')";
+
+    if ($conexao->query($sql) === TRUE){
+        echo "Seu barco foi registrado com sucesso."; // sucesso ao inserir o novo barco no banco de dados
+        exit();
+    } else {
+        echo "Ocorreu um erro ao registrar seu barco, por favor, verifique os valores inseridos e tente novamente." . $conexao->error; // erro ao inserir o barco no banco de dados
+    }
+}
+?>

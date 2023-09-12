597db2bf72200fd6c61c4406d7a932099ebf6134
Sabrinaallgalvao <101464580+Sabrinaallgalvao@users.noreply.github.com>
2023-09-12 16:26:07 -0300

    teste bd azure

--- classes/conexao.php
+++ classes/conexao.php
@@ -1,27 +1,18 @@
 <?php
-
-//Access
 include_once("../classes/conexao.php");
-
-if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-
-    $dbHost     = "localhost";
-    $dbUsername = "esocialu_AlexandreDev45";
-    $dbPassword = "8416RWo{bXM-";
-    $dbName     = "esocialu_ParatyTur";
-
-    
-    $conexao = new mysqli($dbHost, $dbUsername, $dbPassword, $dbName);
+$con = mysqli_init();
+mysqli_ssl_set($con,NULL,NULL, "{path to CA cert}", NULL, NULL);
+mysqli_real_connect($conn, "paratytur.mysql.database.azure.com", "esocialu_AlexandreDev45", "{8416RWo{bXM-}", "{paratytur}", 3306, MYSQLI_CLIENT_SSL);
 
     // Verificar conexão
-    if ($conexao->connect_error) {
-        die("Erro de conexão com o banco de dados: " . $conexao->connect_error);
-    }
-
+if ($conexao->connect_error) {
+    die("Erro de conexão com o banco de dados: " . $conexao->connect_error);
+}
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     // Lógica de verificação de credenciais.
     // Consulta SQL para inserir as credenciais no banco de dados.
 
-    $mysql = "INSERT INTO login (id_login, usuario, senha) VALUES (1, 'adm2', '1234')";
+    $mysql = "INSERT INTO `usuario` (id_usuario, usuario, senha) VALUES (1, 'adm', '1234')";
 
     if (mysqli_query($conexao, $mysql)) {
         // Credenciais inseridas com sucesso.
@@ -39,7 +30,14 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 
 
 
+/*//$mysql = "INSERT INTO 'login' ('id_login', 'usuario', 'senha') VALUES (NULL, 'adm', '1234')";
 
+    //if ($conexao->query($mysql) === TRUE) {
+        //echo "Registro inserido com sucesso.";
+        //header("Location: tela_adm.html");
+        //exit();
+    //} else
+        //echo "Erro ao inserir registro: " . $conexao->error;
 
 
 
@@ -69,5 +67,17 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
         } 
 
     }
+
+
+
+
+
+    $dbHost     = "localhost";
+    $dbUsername = "esocialu_AlexandreDev45";
+    $dbPassword = "8416RWo{bXM-";
+    $dbName     = "esocialu_ParatyTur"; //sem underline no azure sql
+
+    
+    $conexao = new mysqli($dbHost, $dbUsername, $dbPassword, $dbName);
     */
 ?>

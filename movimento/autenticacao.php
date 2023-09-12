442096b8df66369fca7370db4cde0ca418d7ad1d
Sabrinaallgalvao <101464580+Sabrinaallgalvao@users.noreply.github.com>
2023-09-12 14:47:28 -0300

    alterações

--- movimento/autenticacao.php
+++ movimento/autenticacao.php
@@ -1,32 +1,32 @@
 <?php
 include_once("../classes/conexao.php");
 
-$dbHost     = "localhost";
-$dbUsername = "esocialu_AlexandreDev45";
-$dbPassword = "8416RWo{bXM-";
-$dbName     = "esocialu_ParatyTur";
-
-$conexao = new mysqli($dbHost, $dbUsername, $dbPassword, $dbName);
+//Login ADM1
+$usuario1 = $_POST['usuario1'];
+$senha1 = $_POST['senha1'];
+// Login ADM2
+$usuario2 = $_POST['usuario2'];
+$senha2 = $_POST['senha2'];
+//Login ADM3
+$usuario3 = $_POST['usuario3'];
+$senha3 = $_POST['senha3'];
 
 // Verificar conexão
 if ($conexao->connect_error) {
     die("Erro de conexão com o banco de dados: " . $conexao->connect_error);
 }
 
-if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-    $usuario = $_POST['usuario'];
-    $senha = $_POST['senha'];
-
-    // VERIFICA se os dados de login SÃO IGUAIS a 'adm' e '1234'
-    if ($usuario === 'adm' && $senha === '1234') {
-        // SE SIM: redireciona para a tela do administrador
-        header("Location: tela_adm.html");
-        exit();
-    } else {
-        // SE NÃO: redireciona para a tela da pousada
-        header("Location: tela_pousada.html");
-        exit();
-    }
+if (($usuario_digitado === $usuario1 && $senha_digitada === $senha1) ||
+    ($usuario_digitado === $usuario2 && $senha_digitada === $senha2) ||
+    ($usuario_digitado === $usuario3 && $senha_digitada === $senha3)) {
+    // SE SIM: redireciona para a tela do administrador
+    header("Location: tela_adm.html");
+    exit();
+} else {
+    // SE NÃO: redireciona para a tela da pousada
+    header("Location: tela_pousada.php");
+    exit();
 }
+
 $conexao->close();
 ?>

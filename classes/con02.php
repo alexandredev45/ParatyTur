6d6bdb4000d829aed94f601ed22f77b09f07fa47
Alexandre <94768326+alexandredev45@users.noreply.github.com>
2023-09-13 12:56:08 -0300

    .

--- classes/con02.php
+++ classes/con02.php
@@ -1,39 +1,16 @@
-
 <?php
-include_once("../classes/conexao.php");
 
-if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-
-    $dbHost     = "localhost";
-    $dbUsername = "root";
-    $dbPassword = "";
-    $dbName     = "paratytur";
-
-    
-    $conexao = new mysqli($dbHost, $dbUsername, $dbPassword, $dbName);
-
-    // Verificar conexão
-    if ($conexao->connect_error) {
-        die("Erro de conexão com o banco de dados: " . $conexao->connect_error);
-    }
-
-    // Lógica de verificação de credenciais.
-    // Consulta SQL para inserir as credenciais no banco de dados.
-
-    $mysql = "INSERT INTO produto (nome, valor, quantidade, descricao, obs) VALUES ('teste', '10', 5, 'oooooo' '1234')";
-
-    if (mysqli_query($conexao, $mysql)) {
-        // Credenciais inseridas com sucesso.
-        echo "Registro inserido com sucesso.";
-        //header("Location: tela_adm.html"); // Caso esteja tudo certo, redireciona para tela_adm.html
-        exit();
-    } else {
-        // Caso ocorra algum erro ao inserir as credenciais, exiba uma mensagem de erro.
-        echo "Erro ao inserir registro: " . $conexao->error;
-    }
-
-    // Fechar a conexão com o banco de dados
-    $conexao->close();
+	$host = 'mysql:host=localhost;dbname=';
+	$user = "if0_35026416";
+	$senha = "vKnh76PQ7WtjAi";
+	
+try{
+	$conexao = new PDO($host,$user,$senha);
+    $conexao->query($query);
+	
+}
+catch(PDOException $e){
+	print_r($e);
 }
 
 ?>

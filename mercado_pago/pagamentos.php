7af6190ef6daa235a786d7ae3d4cdcaf0dbe5b5e
Alexandre <94768326+alexandredev45@users.noreply.github.com>
2023-09-13 16:36:14 -0300

    .

--- mercado_pago/pagamentos.php
+++ mercado_pago/pagamentos.php
@@ -1,10 +1,8 @@
-<<<<<<< HEAD
-         
 <?php
 
-  MercadoPago\SDK::setAccessToken("ENV_ACCESS_TOKEN");
+  //MercadoPago\SDK::setAccessToken("ENV_ACCESS_TOKEN");
 
-  $pagamento = new MercadoPago\Pagamento();
+  //$pagamento = new MercadoPago\Pagamento();
 
   $pagamento->quantidade_transacao = 100;
   $pagamento->token = "ff8080814c11e237014c1ff593b57b4d";
@@ -17,6 +15,6 @@
   $pagamento->salvar();
 
 ?>
-=======
+
 <?php ?>
->>>>>>> 9cba7d19c0f5c128e14dadd72ff9fd2172e604fc
+

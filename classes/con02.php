cead375af9551aaeb1c36a7876528938cac51f0e
Alexandre <94768326+alexandredev45@users.noreply.github.com>
2023-09-13 09:29:12 -0300

    .

---
+++ classes/con02.php
@@ -0,0 +1,28 @@
+<?php
+
+echo 'Current PHP version :'.phpversion();
+
+
+
+
+// Configurações do banco de dados
+/* 
+$host = 'localhost'; // Host do banco de dados
+$dbname = 'paratytur'; // Nome do banco de dados
+$username = 'dev'; // Nome de usuário do banco de dados
+$password = ']Jkj{qKcDZ+.'; // Senha do usuário do banco de dados
+
+try {
+    // Cria uma conexão PDO
+    $pdo = new PDO("mysql:host=$host;dbname=$dbname", $username, $password);
+
+    // Configura o PDO para lançar exceções em caso de erros
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+    // Agora você pode executar consultas SQL usando $pdo
+    // Exemplo: $pdo->query("SELECT * FROM tabela");
+} catch (PDOException $e) {
+    // Em caso de erro na conexão, você pode tratar a exceção aqui
+    echo "Erro de conexão: " . $e->getMessage();
+}*/
+?>

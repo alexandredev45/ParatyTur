a4eee58e31fda8c174dc51448e160000e2987355
Sabrinaallgalvao <101464580+Sabrinaallgalvao@users.noreply.github.com>
2023-09-13 16:45:48 -0300

    Update cancelamento.php

--- mercado_pago/cancelamento.php
+++ mercado_pago/cancelamento.php
@@ -1,7 +1,7 @@
 <?php
  
 MercadoPago\SDK::setAccessToken("YOUR_ACCESS_TOKEN");
-$pagamento_id = 000000000;Pagamento::find_by_id($pagamento_id);
+$id_pagamento = 000000000;Pagamento::find_by_id($id_pagamento);
 $pagamento->status = "cancelado";
 $pagamento->update();
  

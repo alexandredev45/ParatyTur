20c60211b47cfd70c5c424d6c180ab7e96e26583
Sabrinaallgalvao <101464580+Sabrinaallgalvao@users.noreply.github.com>
2023-09-13 09:25:08 -0300

    .

--- classes/conexao.php
+++ classes/conexao.php
@@ -1,13 +1,19 @@
 <?php
 include_once("../classes/conexao.php");
-$con = mysqli_init();
-mysqli_ssl_set($con,NULL,NULL, "{path to CA cert}", NULL, NULL);
-mysqli_real_connect($conn, "paratytur.mysql.database.azure.com", "esocialu_AlexandreDev45", "{8416RWo{bXM-}", "{paratytur}", 3306, MYSQLI_CLIENT_SSL);
+
+    $dbHost     = "localhost";
+    $dbUsername = "esocialu_AlexandreDev45";
+    $dbPassword = "8416RWo{bXM-";
+    $dbName     = "esocialu_ParatyTur"; //sem underline no azure sql
+    
+    $conexao = new mysqli($dbHost, $dbUsername, $dbPassword, $dbName);
 
     // Verificar conexão
 if ($conexao->connect_error) {
     die("Erro de conexão com o banco de dados: " . $conexao->connect_error);
+    exit();  
 }
+
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     // Lógica de verificação de credenciais.
     // Consulta SQL para inserir as credenciais no banco de dados.
@@ -23,7 +29,6 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
         // Caso ocorra algum erro ao inserir as credenciais, exiba uma mensagem de erro.
         echo "Erro ao inserir registro: " . $conexao->error;
     }
-
     // Fechar a conexão com o banco de dados
     $conexao->close();
 }
@@ -38,8 +43,6 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
         //exit();
     //} else
         //echo "Erro ao inserir registro: " . $conexao->error;
-
-
 
 /*
     define('HOST', 'localhost');
@@ -67,10 +70,6 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
         } 
 
     }
-
-
-
-
 
     $dbHost     = "localhost";
     $dbUsername = "esocialu_AlexandreDev45";

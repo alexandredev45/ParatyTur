7cd379a015a1587bc52ded8c9296aebf8b1cd07d
Alexandre <94768326+alexandredev45@users.noreply.github.com>
2023-09-14 09:58:41 -0300

    .

--- classes/conexao.php
+++ classes/conexao.php
@@ -1,11 +1,20 @@
 <?php
 
+<<<<<<< Updated upstream
     $dbHost     = "localhost";
     $dbUsername = "esocialu_dev";
     $dbPassword = "#paraty556762";
     $dbName     = "esocialu_paratytur";
     
     $conexao = mysqli_connect ($dbHost, $dbUsername,$dbPassword, $dbName);
+=======
+$host = 'sql302.infinityfree.com';
+$username = 'if0_35026416';
+$password = 'vKnh76PQ7WtjAi';
+$database = 'if0_35026416_paratytur';
+
+$conexao = mysqli_connect($host, $username, $password, $database);
+>>>>>>> Stashed changes
 
 
     // Verificar conexão
@@ -15,7 +24,7 @@ if (!$conexao) {
     echo "<h1>SUCESSO</h1>";
 }
 
-if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+if (true) {
     // Lógica de verificação de credenciais.
     // Consulta SQL para inserir as credenciais no banco de dados.
 
@@ -24,55 +33,15 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     if ($conexao->query($mysql) === TRUE) {
         // Credenciais inseridas com sucesso.
         echo "Conexão feita com sucesso.";
-        header("Location: tela_adm.php"); // Caso esteja tudo certo, redireciona para tela_adm.php
+        //header("Location: tela_adm.php"); // Caso esteja tudo certo, redireciona para tela_adm.php
         exit();
     } else {
         // Caso ocorra algum erro ao inserir as credenciais, exiba uma mensagem de erro.
         echo "Falha na conexão com o banco de dados: " . $conexao->error;
     }
-        // Fechar a conexão com o banco de dados
+    // Fechar a conexão com o banco de dados
 
 }
 mysqli_close($conexao);
 
-
-
-
-
-/*//$mysql = "INSERT INTO 'login' ('id_login', 'usuario', 'senha') VALUES (NULL, 'adm', '1234')";
-
-    //if ($conexao->query($mysql) === TRUE) {
-        //echo "Registro inserido com sucesso.";
-        //header("Location: tela_adm.html");
-        //exit();
-    //} else
-        //echo "Erro ao inserir registro: " . $conexao->error;
-
-/*
-    define('HOST', 'localhost');
-    define('DBNAME', 'esocialu_ParatyTur');
-    define('USER', 'esocialu_AlexandreDev45');
-    define('PASSWORD', '8416RWo{bXM-');files-group
-
-    class Connect{
-        protected $connection;
-
-        function __construct(){
-            $this->connectDatabase();
-        }
-
-        private function connectDatabase(){
-            try 
-            {
-                $this->connection = new PDO('mysql:host='.HOST.';dbname='.DBNAME, USER, PASSWORD);
-            } 
-            catch (PDOException $e) 
-            {
-                echo "Error to connect with Database!".$e->getMessage();
-                die();
-            }
-        } 
-
-    }
-    */
 ?>

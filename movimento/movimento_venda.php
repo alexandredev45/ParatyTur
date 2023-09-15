7259eb99230a54bd3a620981e51f752d64624225
Sabrinaallgalvao <101464580+Sabrinaallgalvao@users.noreply.github.com>
2023-09-15 12:21:10 -0300

    Update movimento_venda.php
    PDO

--- movimento/movimento_venda.php
+++ movimento/movimento_venda.php
@@ -1,23 +1,22 @@
 <?php 
-/*include_once("../classes/conexao.php");
+include_once("../classes/conexao.php");
 
-// Verificar conexão
-if ($conexao->connect_error) {
-    die("Erro de conexão com o banco de dados: " . $conexao->connect_error);
-}
-
-//INNER JOIN para solicitar dados das chaves
+try {
+$conexao->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    
+    //INNER JOIN para solicitar dados das chaves
 $mysql = "SELECT v.id_venda, v.id_pousada, v.id_passeio, v.id_barco, v.id_produto, v.id_agenda, p.nome_fantasia
         FROM venda v
         INNER JOIN pousada p ON v.id_pousada = p.id_pousada";
 
-$resultado = $conexao->query($mysql);
+$resultado = $conexao->prepare($mysql);
+$resultado->execute();
 
 if ($resultado->num_rows > 0) {
     echo "<table>";
     echo "<tr><th>Venda</th>Pousada</th><th>Nome Fantasia da Pousada</th><th>Passeio</th><th>Barco</th><th>Produto</th><th>Agenda</th></tr>";
 
-    while ($linha = $resultado->fetch_assoc()) {
+    while ($linha = $resultado->fetch(PDO::FETCH_ASSOC)) {
         echo "<tr>";
         echo "<td>" . $linha['id_venda'] . "</td>";
         echo "<td>" . $linha['id_pousada'] . "</td>";
@@ -29,11 +28,13 @@ if ($resultado->num_rows > 0) {
         echo "</tr>";
     }
     echo "</table>";
-
 } else {
     echo "Nenhuma venda registrada.";
 }
 
 // Fechar a conexão com o banco de dados
-$conexao->close();*/
+$conexao = null;
+} catch (PDOException $e) {
+    echo "Erro: " . $e->getMessage();
+}
 ?>

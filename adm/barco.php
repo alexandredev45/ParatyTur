3df71ce7e3fcf811a623b08b55167531b605255f
Alexandre <94768326+alexandredev45@users.noreply.github.com>
2023-09-18 09:34:36 -0300

    :alien:

--- adm/barco.php
+++ adm/barco.php
@@ -10,16 +10,53 @@
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-HwwvtgBNo3bZJJLYd8oVXjrBZt8cqVSpeBNS5n7C8IVInixGAoxmnlMuBnhbgrkm" crossorigin="anonymous"></script>
     <title>Administrador</title>
 </head>
+<style>
+  main{
+    margin: 1rem 3rem;
+  }
+</style>
 <body>
 <header>
   <?php 
-  include_once "../adm/menu.php";
+    include_once "../adm/menu.php";
   ?>
 </header>
 
 <?php 
 echo "Barco";
 ?>
+<main>
+<table class="table table-striped">
+  <thead>
+    <tr class=>
+      <th scope="col">#</th>
+      <th scope="col">Nome</th>
+      <th scope="col">Proprietario</th>
+      <th scope="col">Comprimento</th>
+      <th scope="col">Capacidade</th>
+      <th scope="col">Ano</th>
+      <th scope="col">+</th>
+      <th scope="col">...</th>
+      <th scope="col">...</th>
+    </tr>
+  </thead>
+  <tbody>
+    <tr>
+      <th scope="row">1</th>
+      <td>...</td>
+      <td>...</td>
+      <td>...</td>
+      <td>...</td>
+      <td>...</td>
+      <td>...</td>
+      <td>...</td>
+      <td>...</td>
+    </tr>
+    
+  
+  </tbody>
+</table>
+</main>
 
 </body>
 </html>

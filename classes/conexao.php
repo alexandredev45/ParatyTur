19a56b58d07740fb3c1e1fffcb67a0c5b63f099d
Sabrinaallgalvao <101464580+Sabrinaallgalvao@users.noreply.github.com>
2023-09-18 08:31:37 -0300

    Update conexao.php

--- classes/conexao.php
+++ classes/conexao.php
@@ -3,7 +3,7 @@
 $host = 'mysql:host=ns950.hostgator.com.br;dbname=esocialu_paratytur';
 $user = 'esocialu_dev';
 $senha = 'Hi~g$1X3#u6F';
-	
+	  
 $senhaHash = password_hash($senhaOriginal, PASSWORD_DEFAULT);
 
 try{

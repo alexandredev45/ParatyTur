ccd03ef44e87f089bb9026896855b48d7d5975da
Alexandre <94768326+alexandredev45@users.noreply.github.com>
2023-10-05 16:15:45 -0300

    :alien:

--- js/scroll.js
+++ js/scroll.js
@@ -17,7 +17,7 @@ const debounce = function(func, wait, immediate) {
   const animationClass = 'animate';
   
   function animeScroll() {
-    const windowTop = window.pageYOffset + ((window.innerHeight * 2) / 4);
+    const windowTop = window.pageYOffset + ((window.innerHeight * 2) / 2);
     target.forEach(function(element) {
       if((windowTop) > element.offsetTop) {
         element.classList.add(animationClass);
